5bbdfb93bdb1f1420193e545e2afbe55fcdcba17
Jeanette Huang <Jeanette@TonyStark.local>
2017-01-15 18:29:36 -0800

    test 2

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -58,12 +58,12 @@
 		// 	$(".screen-height").height($(window).height());
 		// });
 
-		if (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)) {
-			$('#home').css({'background-attachment': 'scroll'});
-		} else {
-			$('#home').parallax('50%', 0.1);
-		}
-
+		// if (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)) {
+		// 	$('#home').css({'background-attachment': 'scroll'});
+		// } else {
+		// 	$('#home').parallax('50%', 0.1);
+		// }
+		//
 
 		/* ---------------------------------------------- /*
 		 * WOW Animation When You Scroll
